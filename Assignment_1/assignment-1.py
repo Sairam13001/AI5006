6d0fc86f5fcf5e3837eb748b1790207481ad961e
Sairam13001 <70787552+Sairam13001@users.noreply.github.com>
2020-09-06 12:15:26 +0530

    Update assignment-1.py

--- Assignment_1/assignment-1.py
+++ Assignment_1/assignment-1.py
@@ -1,47 +1,44 @@
-# Given two parallel planes P1: a1 * x + b1 * y + c1 * z + d1 = 0 and P2: a2 * x + b2 * y + c2 * z + d2 = 0.
+# Given two parallel planes P1: ax+ by + cz + d1 = 0 and P2: ax + by + cz + d2 = 0.
 # This is a program to find distance between these planes
-# Distance = (| a*x1 + b*y1 + c*z1 + d |) / (sqrt( a*a + b*b + c*c))
+# Distance = (|d1 - d2|)/sqrt(a*a + b*b +c*c)
 
 from math import sqrt
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-#Program for plotting the two parallel planes
-a1,b1,c1,d1 = 2,3,4,4
-a2,b2,c2,d2 = 4,6,8,12
+def plot_planes(a1,b1,c1,d1,a2,b2,c2,d2):
+    x = np.linspace(-1,1,20)
+    y = np.linspace(-1,1,20)
 
-x = np.linspace(-1,1,20)
-y = np.linspace(-1,1,20)
+    X,Y = np.meshgrid(x,y)
+    Z1 = (d1 - a1*X - b1*Y) / c1
+    Z2 = (d2 - a2*X - b2*Y) / c2
 
-X,Y = np.meshgrid(x,y)
-Z1 = (d1 - a1*X - b1*Y) / c1
-Z2 = (d2 - a2*X - b2*Y) / c2
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
 
-fig = plt.figure()
-ax = fig.gca(projection='3d')
+    ax.set_xlabel('x axis')
+    ax.set_ylabel('y axis')
+    ax.set_zlabel('z axis')
 
-ax.set_xlabel('x axis')
-ax.set_ylabel('y axis')
-ax.set_zlabel('z axis')
+    surf1 = ax.plot_surface(X, Y, Z1)
+    surf2 = ax.plot_surface(X, Y, Z2)
 
-surf1 = ax.plot_surface(X, Y, Z1)
-surf2 = ax.plot_surface(X, Y, Z2)
+    plt.title('Assignment-1 : Parallel Planes')
+    plt.show()
 
-plt.title('Assignment-1 : Parallel Planes')
-plt.show()
-
-#Program for finding the distance
 def find_distance_bw_planes(a1,b1,c1,d1,a2,b2,c2,d2):
     #To check if planes are parallel
     if (a1/a2 == b1/b2 == c1/c2):
-        #consider y1 = z1 = 0
-        x1 = -d1/a1
-        n = np.array([a2,b2,c2])
-        distance = abs(a2*x1 + d2)/np.linalg.norm(n)
+        n1 = np.array([a1,b1,c1,d1])
+        n2 = np.array([a2,b2,c2,d2])
+        n1 = n1*(a2/a1) 
+        distance = abs(n1[3] - n2[3])/np.linalg.norm(n1[:3])
     else:
         print("The given planes are not parallel")
         return -1
     return distance
 
+plot_planes(2,3,4,-4,4,6,8,-12)    
 print("The distance between the given parallel planes is : ", find_distance_bw_planes(2,3,4,-4,4,6,8,-12))
